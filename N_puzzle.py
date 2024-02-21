7f7405f5345ce10954ad75d490b3fe150da77fb0
ramo2004 <125517101+ramo2004@users.noreply.github.com>
2024-02-21 11:08:12 -0600

    Add files via upload
    N doors are present in a row that are all initially closed.
    • Make N passes by each of these doors
    • The first time through, visit every door and ‘toggle’ the door (if the door is closed, open
    it; if it is open, close it).
    • The second time, only visit every 2nd door (i.e., door #2, #4, #6, ...) and toggle it.
    • The third time, visit every 3 rd door (i.e., door #3, #6, #9, ...), etc., until you only visit the
    N th door on the N th pass.

---
+++ N_puzzle.py
@@ -0,0 +1,28 @@
+
+print("Tibian Elsheikh, CS3343 Project 1\n")
+
+def n_doors_puzzle(N):
+    doors = [False] * N  
+
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if j % i == 0:
+                doors[j - 1] = not doors[j - 1]  
+                print(f"i: {i}; j: {j}; step size: {i}. Toggling door number {j}.")
+
+    print("Algorithm has finished.\n")
+
+    open_doors = [door_number + 1 for door_number, state in enumerate(doors) if state]
+    print("\n".join(f"Door number {door} remains open." for door in open_doors))
+    print("\n")
+
+
+    closed_doors = [door_number + 1 for door_number, state in enumerate(doors) if not state]
+    print("\n".join(f"Door number {door} remains closed." for door in closed_doors))
+
+
+N1 = int(input("Run 1:\nEnter the number of Doors (N): "))
+n_doors_puzzle(N1)
+
+N2 = int(input("\nRun 2:\nEnter the number of Doors (N): "))
+n_doors_puzzle(N2)
